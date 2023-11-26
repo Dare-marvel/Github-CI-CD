f8308571d06d74409f0ceb47d7feec322faf846d
adwaitp <adwait.purao@spit.ac.in>
2023-11-26 11:14:30 +0530

    implemented all other methods

--- src/calculator/divide.js
+++ src/calculator/divide.js
@@ -1,3 +1,10 @@
-/**
- * Division
- */
+function divide(firstNum, secondNum) {
+    if (secondNum !== 0) {
+      return firstNum / secondNum;
+    } else {
+      return "Cannot divide by zero";
+    }
+  }
+  
+  module.exports = divide;
+  
